88cd69b6208522e3dfbd0fec26668a25ed6ed747
Dirley Rodrigues <dirleyrls@gmail.com>
2015-10-07 16:50:18 -0300

    Compile i18n messages during deploy.
    Here we introduce a new post-compile hook to run `manage.py compilemessages`
    for each one of our apps. This depends on the presence of the GNU gettext
    binaries in the production server. To achieve that, we use our
    [heroku-buildpack-gettext](https://github.com/labhackercd/heroku-buildpack-gettext).
    
    See [Heroku's documentation on multiple
    buildpacks](https://devcenter.heroku.com/articles/using-multiple-buildpacks-for-an-app)
    for more information about using multiple buildpacks.
    
    In our specific case, we need to make sure the order of buildpacks is:
    
    1. heroku-buildpack-gettext
    2. heroku-buildpack-python
    
    This way gettext will be available during python's compile, which is where our
    post-compile hook will run.

---
+++ scripts/post-compile/04-compilemessages.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+indent() {
+    RE="s/^/       /"
+    [ $(uname) == "Darwin" ] && sed -l "$RE" || sed -u "$RE"
+}
+
+MANAGE_FILE=$(find . -maxdepth 3 -type f -name 'manage.py' | head -1)
+MANAGE_FILE=${MANAGE_FILE:2}
+
+echo "-----> Compiling messages"
+
+export PATH=$PATH:/app/.vendor/gettext/bin
+export LD_LIBRARY_PATH=/app/.vendor/gettext/lib
+
+MANAGE_PATH="$(pwd)/$MANAGE_FILE"
+BASE_DIR=$(dirname "$MANAGE_PATH")
+
+pushd "$BASE_DIR" > /dev/null && find . -type d -name locale -not -path '*/\.*' -print0 | while IFS= read -r -d $'\0' locale; do
+    cd "$BASE_DIR/$locale/.." && python "$MANAGE_PATH" compilemessages
+done
+
+popd > /dev/null
+
+echo
+
