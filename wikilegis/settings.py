748347f16f3b9fd6cb932e54ffe48365bb7fd903
erivanio <erivanio.vanin@gmail.com>
2016-07-01 10:22:58 -0300

    add DEFAULT_FROM_EMAIL in settings

--- wikilegis/settings.py
+++ wikilegis/settings.py
@@ -176,6 +176,7 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
 
 # python-social-auth: http://psa.matiasaguirre.net/docs/index.html
 
